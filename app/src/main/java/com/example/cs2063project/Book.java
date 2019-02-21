160809caee3014fdfaefb315c531e911ffba1928
Tyler Sargent <tsargent@Tylers-MacBook-Pro.local>
2019-02-20 22:28:56 -0400

    Added layout and partial functionality for creating books.

--- app/src/main/java/com/example/cs2063project/Book.java
+++ app/src/main/java/com/example/cs2063project/Book.java
@@ -1,4 +1,15 @@
 package com.example.cs2063project;
 
-public class Book {
+class Book {
+    String title;
+    String author;
+    String pageCount;
+   // int photoId;
+
+    Book(String title, String author, String pageCount) {
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+       // this.photoId = photoId;
+    }
 }
--- app/src/main/java/com/example/cs2063project/MyBooksActivity.java
+++ app/src/main/java/com/example/cs2063project/MyBooksActivity.java
@@ -9,14 +9,23 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyBooksActivity extends AppCompatActivity
 
             implements NavigationView.OnNavigationItemSelectedListener {
+
+        private List<Book> books;
+        private RecyclerView rv;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,25 @@ public class MyBooksActivity extends AppCompatActivity
 
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(this);
+            rv=(RecyclerView)findViewById(R.id.rv);
+
+            LinearLayoutManager llm = new LinearLayoutManager(this);
+            rv.setLayoutManager(llm);
+            rv.setHasFixedSize(true);
+
+            Button addBookButton = (Button) findViewById(R.id.add_book);
+            books = new ArrayList<>();
+            addBookButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    books.add(new Book("Harry Potter", "JK Rowling", "512"));
+                    initializeAdapter();
+                }
+            });
+        }
+        private void initializeAdapter(){
+            MyBooksAdapter adapter = new MyBooksAdapter(books);
+            rv.setAdapter(adapter);
         }
 
         @Override
@@ -73,7 +101,7 @@ public class MyBooksActivity extends AppCompatActivity
             // Handle navigation view item clicks here.
             int id = item.getItemId();
 
-            Intent intent = null;
+            Intent intent;
             if (id == R.id.nav_welcome) {
                 // Handle the camera action
                 intent = new Intent(this,WelcomeActivity.class);
